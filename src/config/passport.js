d5daec10a646ef0a0a3004530e1cba5309b84b11
anbuleo <anbuliyon@gmail.com>
2025-04-30 00:10:08 -0700

    passport

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,7 +11,7 @@ env.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: "https://servybackend.onrender.com/api/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const response = await fetch(
